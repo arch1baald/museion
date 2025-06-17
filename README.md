565ac518b7b32778ab70bd364b03965cc7a1d164
Dmitry Savostyanov <dmsavostyanov123@gmail.com>
2025-06-17 00:34:55 -0700

    feat(cli): ucli skeleton

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # museion
 From-Scratch text-to-image using Latent Diffusion
+
+
+## Installation
+
+```bash
+uv sync
+```
+
+## Usage
+
+```bash
+./ucli --help
+```
+
+or
+```bash
+alias ucli='./ucli'
+
+ucli --help
+```
---
+++ src/museion/cli.py
@@ -0,0 +1,46 @@
+import logging
+
+import typer
+from typing_extensions import Annotated
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)  # TODO: logging config
+
+app = typer.Typer(
+    pretty_exceptions_enable=True,
+    pretty_exceptions_show_locals=False,
+)
+
+
+@app.callback()
+def set_log_level(
+    verbose: Annotated[int, typer.Option("--verbose", "-v", count=True)] = 0,
+    quiet: Annotated[int, typer.Option("--quiet", "-q", count=True)] = 0,
+) -> None:
+    level = logging.INFO
+    if verbose > 0:
+        level = logging.DEBUG
+    elif quiet > 2:
+        level = logging.CRITICAL
+    elif quiet > 1:
+        level = logging.ERROR
+    elif quiet > 0:
+        level = logging.WARN
+
+    root = logging.getLogger()
+    root.setLevel(level)
+
+
+@app.command(name="echo", help="Echo a message")
+def app_echo(text: str = "Hello, World!") -> None:
+    logger.info("Echoing message: %s", text)
+    typer.echo(text)
+
+
+@app.command(name="echo2", help="Echo a message")
+def app_echo2(text: str = "Hello, World!") -> None:
+    typer.echo(text)
+
+
+if __name__ == "__main__":
+    app()
